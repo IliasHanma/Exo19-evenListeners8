8a247e8b60b55f8b1def6fe8d51656b63b995731
iliaselboazzati <ilias-elboazzati@hotmail.com>
2021-06-03 16:22:07 +0200

    Exo19 evenListeners8

---
+++ main.js
@@ -0,0 +1,56 @@
+// <!-- > Event Target -->
+
+let pContent = "Il est possible de passer des parametres dans la function utilisé dans l'écouteur d'événement !";
+ let theColor = "red";
+ let theBackground = "gold";
+ let myP = document.querySelector('p');
+ console.log(myP);
+ let btn = document.querySelector('button');
+ 
+
+// <!-- ### 1. Créer un programme qui te permet de passer le contenu de la variable 
+// pContent jusqu'a une function et en suite fait en sorte que cette function
+//  ajoute ce qu'elle a reçue en parametre dans le p de la section au click du bouton.
+// let replace = (p,contenu) =>{
+//    p.innerText = contenu.innerText ;
+// }
+
+// btn.addEventListener('click',() =>{
+//     replace(myP,pContent);
+// });
+let myP = document.querySelector("p");
+
+let myButton = document.querySelector("button");
+
+let maFonc = (replace) => {
+    myP.innerText = replace;
+
+}
+
+myButton.addEventListener("click", () =>{
+    maFonc(pContent)
+})
+
+
+
+// ### 2. Créer un programme qui au clique du h1, le background devient gold et sa color red, 
+// Attention voici les contraintes :
+// - Les couleurs sont contenu dans des variables sur le fichier main.js,
+//  sert-en pour les affecter au style de la balise h1
+// - Il faut que ces variables soient envoyé en parametre dans
+//  la function via le addEventListener. -->
+
+
+let theColor = "red";
+let theBackground = "gold";
+
+let h1 = document.querySelector("h1");
+
+let maFonc2 = (bg, color) => {
+    h1.style.backgroundColor = bg;
+    h1.style.color = color;
+}
+
+h1.addEventListener("click", () =>{
+    maFonc2(theBackground, theColor)
+})
